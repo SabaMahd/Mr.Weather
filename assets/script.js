e95140b377ce0b626c1b9ae0102bef9997fa76ae
saba mahd <sabamahd.sm@gmail.com>
2022-04-18 19:29:39 -0400

    scrpt/css files made

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+const app = {
+    init: () => {
+      document
+      .getElementByID('btnGet')
+      .gitEventListener('click',app.getlocation);
+    
+      fetchWeather: (ev) => {
+        
+      }
